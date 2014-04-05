17afd233e246cb2968a71e5f03717c4a1cb6c72b
Gureya Daharewa Davis <gureya.daharewa@gmail.com>
2014-04-05 11:50:37 -0700

    Added CommitRequestStatus.cs

---
+++ SOURCE/PADI_DTMS/PADI_LIBRARY/UTIL_CLASSES/CommitRequestStatus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PADI_LIBRARY
+{
+    class CommitRequestStatus
+    {
+        private ObjectServer server;
+
+        public ObjectServer Server
+        {
+            get { return server; }
+            set { server = value; }
+        }
+
+        private bool vote;
+
+        public bool Vote
+        {
+            get { return vote; }
+            set { vote = value; }
+        }
+
+        private bool hasCommited;
+
+        public bool HasCommited
+        {
+            get { return hasCommited; }
+            set { hasCommited = value; }
+        }
+
+    }
+}
